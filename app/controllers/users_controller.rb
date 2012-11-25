b7496ed7487525ed22c13fae68286ee4bd40163f
Apprentice <apprentice@dbc14.local>
2012-11-25 00:27:10 -0800

    Added profile pages with gravatar as well as user signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+  end
+
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new
+  	render 'new'
   end
 end
